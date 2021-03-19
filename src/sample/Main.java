c7a2a1be2d09676ce72ba3b1000fe0643c4ec0cc
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-03-19 19:41:03 +0000

    Testing 3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("This is JJDL v15");
+        primaryStage.setTitle("This is JJDL v15 ");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
