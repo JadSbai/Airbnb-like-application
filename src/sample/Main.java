4aff4fec37bb041de3ec34e98b2eb440d930911f
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-03-19 18:24:39 +0000

    hi!

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("This is JJDL");
+        primaryStage.setTitle("This is JJDL live from London");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
