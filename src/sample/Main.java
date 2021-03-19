fbdb16b51a263a064f1c414eec92ab4fd9982ef1
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-03-19 18:41:30 +0000

    Testing

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("This is JJDL live from London");
+        primaryStage.setTitle("This is JJDL from 159 wulfstan street");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
