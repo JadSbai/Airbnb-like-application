bbf57008f1b6a81a5a42ceb54758c5cca06a7299
JadSbai <78962232+JadSbai@users.noreply.github.com>
2021-03-22 19:42:17 +0100

    Safe commit 2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("This is JJDL v15: Jad's branch");
+        primaryStage.setTitle("AirBnB");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
