3114c23a397dbde18f5703240559a3d6ac8226d4
lclarkg18 <liam.clark_gutierrez@kcl.ac.uk>
2021-03-26 09:48:32 +0000

    New window pop's up when map is clicked

--- src/sample/PropertyListController.java
+++ src/sample/PropertyListController.java
@@ -1,8 +1,14 @@
 package sample;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class PropertyListController {
@@ -16,13 +22,17 @@ public class PropertyListController {
     private ArrayList<AirbnbListing> listings;
 
 
-    public PropertyListController(String borough){
-        listView = new ListView();
-        listings = loader.loadFromBoruogh(borough);
-        for(AirbnbListing listing: listings){
-
-            listView.getItems().add("Item 1");
-        }
-        hbox = new HBox(listView);
+    public PropertyListController(String boroughAbbreviation) throws IOException {
+        //listView = new ListView();
+        loader = new AirbnbDataLoader();
+        listings = loader.loadFromBoruogh(boroughAbbreviation);
+        //for(AirbnbListing listing: listings){
+        //    listView.getItems().add("Item 1");
+        //}
+        //hbox = new HBox(listView);
+        FXMLLoader propertyList = new FXMLLoader(getClass().getResource("AirbnbViewer.fxml"));
+        Stage stage = propertyList.load();
+        stage.setTitle("AirBnB's in " + boroughAbbreviation);
+        stage.show();
     }
 }
