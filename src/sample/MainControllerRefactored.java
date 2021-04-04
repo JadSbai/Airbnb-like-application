9d1db37a737ea07ed1bccffab9d47942d7d143b1
JadSbai <78962232+JadSbai@users.noreply.github.com>
2021-04-04 17:49:22 +0100

    Commit 2: Controller abstract class

---
+++ src/sample/MainControllerRefactored.java
@@ -0,0 +1,12 @@
+package sample;
+
+public abstract class MainControllerRefactored extends Controller
+{
+    public void initialize()
+    {
+
+    }
+
+    public abstract void rightButtonAction();
+
+}
